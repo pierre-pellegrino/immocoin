da408c530a7f58a683d722b986e95aa9691fd5e1
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-07 15:55:49 +0100

    axios register login logout

--- pages/api/axiosMethods.js
+++ pages/api/axiosMethods.js
@@ -8,40 +8,25 @@ API.interceptors.request.use(({ headers, ...config }) => ({
   headers: {
     ...headers,
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${headers.Authorization ||  Cookies.get('token')}`,
+    Authorization: `${headers.Authorization ||  Cookies.get('token')}`,
   },
 }));
 
 export default class APIManager {
 
   static async register(payload) {
-    const response = await API.post(`/users`, payload);
-    console.log('>>> REGISTER response: ', response)
-    return response.headers.authorization.split(' ')[1];
+    const response = await API.post(`/users`, payload)
+    console.log('>>> REGISTER response.headers.auth: ', response.headers.authorization)
+    Cookies.set('token', response.headers.authorization)
   }
   static async logIn(payload) {
-    const response = await API.post(`/users/sign_in`, payload);
+    const response = await API.post(`/users/sign_in`, payload)
     console.log('>>> LOGIN response: ', response)
-    return response;
+    Cookies.set('token', response.headers.authorization)
   }
   static async logOut() {
-    const response = await API.delete(`/users/sign_out`);
+    const response = await API.delete(`/users/sign_out`)
     console.log('>>> LOGOUT response: ', response)
-    return response;
+    Cookies.remove('token')
   }
-  // static async getProperty() {
-  //   const response = await API.get(API_URL);
-  //   console.log('>>> getProperty response: ', response)
-  //   return response.data;
-  // }
-  // static async editProperty(id, newProperty) {
-  //   const response = await API.put(`/Property/${id}`, newProperty);
-  //   console.log('>>> editProperty response: ', response)
-  //   return response.data;
-  // }
-  // static async deleteProperty(id) {
-  //   const response = await API.delete(`/Property/${id}`);
-  //   console.log('>>> deleteProperty response: ', response)
-  //   return response.data;
-  // }
 }
