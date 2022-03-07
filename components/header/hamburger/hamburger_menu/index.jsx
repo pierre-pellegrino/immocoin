8f207fb41985e8dbb18f9905a6d8663296b50db3
Beygs <boris.gilles@hotmail.fr>
2022-03-07 18:57:16 +0100

    styling done

--- components/header/hamburger/hamburger_menu/index.jsx
+++ components/header/hamburger/hamburger_menu/index.jsx
@@ -1,26 +1,58 @@
 import Image from "next/image";
-import { navItems, menu, profilePicture } from "./hamburger_menu.module.scss";
+import {
+  navItems,
+  menu,
+  profilePictureWrapper,
+  profilePicture,
+  profile,
+} from "./hamburger_menu.module.scss";
 
 const HamburgerMenu = ({ connected = true }) => {
   let content = (
     <>
-      <li>Me connecter</li>
-      <li>M&apos;inscrire</li>
+      <li>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path d="M 6.5625 5.0136719 C 2.4595703 5.2668613 -0.68726562 9.0536406 0.13085938 13.369141 C 0.65285938 16.124141 2.8748594 18.347141 5.6308594 18.869141 C 9.378008 19.579519 12.720128 17.298793 13.703125 14 L 18 14 L 18 15 C 18 16.105 18.895 17 20 17 C 21.105 17 22 16.105 22 15 L 22 14 C 23.105 14 24 13.105 24 12 C 24 10.895 23.105 10 22 10 L 13.699219 10 C 12.979424 7.5432523 10.909496 5.6120152 8.3691406 5.1308594 C 7.7527656 5.0139844 7.1486328 4.977502 6.5625 5.0136719 z M 7 9 C 8.657 9 10 10.343 10 12 C 10 13.657 8.657 15 7 15 C 5.343 15 4 13.657 4 12 C 4 10.343 5.343 9 7 9 z"></path>
+        </svg>
+        Me connecter
+      </li>
+      <li>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path d="M 19.171875 2 C 18.448125 2 17.724375 2.275625 17.171875 2.828125 L 16 4 L 20 8 L 21.171875 6.828125 C 22.275875 5.724125 22.275875 3.933125 21.171875 2.828125 C 20.619375 2.275625 19.895625 2 19.171875 2 z M 14.5 5.5 L 3 17 L 3 21 L 7 21 L 18.5 9.5 L 14.5 5.5 z"></path>
+        </svg>
+        M&apos;inscrire
+      </li>
     </>
   );
 
   if (connected) {
     content = (
       <>
-        <li>
-          <Image
-            src="/default_avatar.svg"
-            width="48"
-            height="48"
-            alt="profile picture"
-            className={profilePicture}
-          />
-          Mon&nbsp;Profil
+        <li className={profile}>
+          <div className={profilePictureWrapper}>
+            <Image
+              src="/default_avatar.svg"
+              width="96"
+              height="96"
+              alt="profile picture"
+              className={profilePicture}
+            />
+          </div>
+          <div>Mon&nbsp;Profil</div>
         </li>
         <hr />
         <li>
