376b64f4c044e8697ce82f470dfce1a53073d987
Pierre <pierre.pellegrino@laposte.net>
2022-03-08 16:55:20 +0100

    Added owner informations on property view

--- components/PropertyView/PropertyView.jsx
+++ components/PropertyView/PropertyView.jsx
@@ -1,20 +1,43 @@
-import {propertyViewPicture} from './property_view.module.scss'
-import Image from 'next/image';
+import {propertyViewPicture, propertyViewWrapper, propertyViewOwner, top, bottom} from './property_view.module.scss'
+import { useAtom } from 'jotai';
+import { isConnectedAtom } from "store";
 
 const PropertyView = ({property, user}) => {
-  const {id, title, description, picture, price} = property;
+  const [isConnected] = useAtom(isConnectedAtom);
+  const {title, description, picture, price, address} = property;
   const {email, first_name, last_name, avatar} = user;
   const cleanPrice = new String(price).replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-  console.log(cleanPrice)
 
   return (
-    <>
-      <h1>{title}</h1>
-      <h2 className="primary">{cleanPrice} €</h2>
-      <div className={propertyViewPicture}>
-        <img src={picture ? picture : "/nopic.svg"} alt={`${first_name}'s property on immocoin`}/>
+    <div className={propertyViewWrapper}>
+
+      <div className="property">
+        <h1> Annonce : </h1>
+        <h3>{title}</h3>
+        <h2 className="primary">{cleanPrice} €</h2>
+        <div className={propertyViewPicture}>
+          <img src={picture ? picture : "/nopic.svg"} alt={`${first_name}'s property on immocoin`}/>
+        </div>
+
+        <h3>Descriptif du bien : </h3>
+        <p>
+          {description}
+        </p>
       </div>
-    </>
+      <div className={propertyViewOwner}>
+        <div className={top}>
+          <h1> Contacter le vendeur </h1>
+        </div>
+        <div className={bottom}>
+          <img src={avatar ? avatar : "/default_avatar.svg"} alt={`${first_name} profile picture`}/>
+          <p className="bolder"> M. {first_name} {last_name} </p>
+          <p className="mb1">{isConnected ? `Email du vendeur: ${email}` : "Connectez-vous pour accéder aux coordonnées du vendeur."}</p>
+          <p className="bolder">Adresse du bien :</p>
+          <p>{isConnected ? address : "Connectez-vous pour accéder aux coordonnées du vendeur."}</p>
+
+        </div>
+      </div>
+    </div>
   );
 };
 
