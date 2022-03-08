f75b3d83868991a2071700ca8802aadbcfbf2fac
Beygs <boris.gilles@hotmail.fr>
2022-03-08 16:16:00 +0100

    lang=fr added to html

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="fr">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Karla:wght@300;400;600;700&family=Work+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" 
