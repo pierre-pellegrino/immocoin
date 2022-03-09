a00c01887456b653c6644fe7c53a2c963f202f48
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-09 16:20:50 +0100

    adds modal edit profile

--- components/ProfilePage/ProfilePage.jsx
+++ components/ProfilePage/ProfilePage.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import {profilePage, profilePage__hero, profilePage__content, profilePage__content__img, left, right} from './profile_page.module.scss';
+import React, { useEffect, useState, useRef } from 'react';
+import {profilePage, profilePage__hero, profilePage__content, profilePage__content__img, left, right } from './profile_page.module.scss';
 import PropertiesList from '../PropertiesList/PropertiesList';
 import {useAtom} from 'jotai';
 import {userAtom} from 'store';
 import Image from 'next/image';
+import Modal from 'react-modal';
+import APIManager from 'pages/api/axiosMethods';
 
 const ProfilePage = ({properties}) => {
   const [user] = useAtom(userAtom);
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+  const avatar = useRef();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const handleSave = async () => {
+    const newProfile = { 
+      firstname: firstname,
+      lastname: lastname,
+      avatar: avatar.current.files[0]
+    }
+    console.log('newProfile: ', newProfile)
+    const response = await APIManager.editProfile(user.id, newProfile)
+    console.log('response: ', response)
+  }
 
   return (
     <div className={profilePage}>
@@ -29,14 +46,56 @@ const ProfilePage = ({properties}) => {
           <p className="mb1"> Nom : {user?.last_name ?? "Non renseigné"} </p>
           <p className="mb1"> Prénom : {user?.first_name ?? "Non renseigné"} </p>
           <p className="mb1"> Email : {user?.email ?? "Non renseigné"} </p>
+          <button onClick={() => setModalIsOpen(true)}>Editer mon profil</button>
         </div>
         <div className={right}>
             <p className="mb1 bolder">Mes annonces</p>
             <PropertiesList properties={properties} profile/>
         </div>
       </div>
+
+      <Modal 
+          style={customStyles}
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <label>Nouveau prénom</label>
+          <input 
+            type="text"
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)} 
+          />
+
+          <label>Nouveau nom</label>
+          <input 
+            type="text"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)} 
+          />
+
+          <label>Avatar</label>
+          <input 
+            type="file"
+            accept="image/png, image/jpeg"
+            ref={avatar}
+          />
+          <br />
+          <button onClick={() => handleSave()}>Enregistrer</button>
+      </Modal>
     </div>
   );
+};
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
 };
 
 export default ProfilePage;
