5a233799dd262a1dd326ccbb5e2487fe1c0d3b0d
Beygs <boris.gilles@hotmail.fr>
2022-03-10 10:21:10 +0100

    reverted the revert

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,26 @@
 import LoginForm from 'components/LoginForm/LoginForm';
 import styles from 'styles/Home.module.css'
 
-
-const login = () => {
+const login = ({ redirected }) => {
   return (
     <main className={styles.main}>
-      <LoginForm />
+      <LoginForm redirected={redirected} />
     </main>
   )
 }
 
 export default login;
+
+export const getServerSideProps = async (context) => {
+  if (context.query.redirected === "true") {
+    return {
+      props: {
+        redirected: true,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
